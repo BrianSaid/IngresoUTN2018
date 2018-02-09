ba16686a700ecbbc77e6349aa10c64c74038d41e
BrianSaid <36137327+BrianSaid@users.noreply.github.com>
2018-02-09 00:17:32 -0300

    Add files via upload

---
+++ 2-InstruccionIf/2-InstruccionIf/jsInstruccionIF (2).js
@@ -0,0 +1,13 @@
+function Mostrar()
+{
+//tomo la edad  
+    var años   
+
+        años = document.getElementById("edad").value;
+
+            if (años >= 18)
+{
+                alert("Usted es mayor de edad");
+}
+
+}//FIN DE LA FUNCIÓN
---
+++ 2-InstruccionIf/2-InstruccionIf/jsInstruccionIF (6).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+//tomo la edad  
+var años   
+
+        años = document.getElementById("edad").value;
+
+if (años <13)
+{
+    alert("Usted es un niño");
+}
+else if (años >17)
+{
+    alert("Usted es mayor");
+}
+else
+{
+    alert("Usted es adolecente")
+}
+
+}//FIN DE LA FUNCIÓN

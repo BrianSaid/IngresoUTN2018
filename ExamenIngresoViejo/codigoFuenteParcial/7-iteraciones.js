ee617aeda00fe5ed29e148528998a56e070c7202
BrianSaid <36137327+BrianSaid@users.noreply.github.com>
2018-03-01 12:04:29 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
@@ -1,6 +1,26 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+    var notas;
+    var notaBaja;
+    var CantVarone;
+    var sexo;
+    var contador=0;
+    var flag =0;
+
+    sexo = prompt ("Ingrese Sexo: ");
+    notas = prompt("Ingrese nota: ");
+
+    if(nota >= 6 && sexo == M)
+    {
+        contador++;
+    }
+    
+
+    
+
+
+
+
 }
 
